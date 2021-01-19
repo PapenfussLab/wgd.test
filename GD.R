5d4a704587ca02216abe5be8a68f611ac3ba7a47
Edwin Sutanto <edwin.sutanto@student.unimelb.edu.au>
2021-01-19 22:31:16 +1100

    clean up main script

--- GD.R
+++ GD.R
@@ -3,10 +3,6 @@
 # Author: Lachlan Mcintosh
 # Conceived by: Lachlan Mcintosh and Tony Papenfuss
 
-parent_dir <- "."
-all_files <- list.files(parent_dir)
-all_files <- all_files[[1]]
-all_files
 library(ggplot2)
 library(gridExtra)
 
@@ -231,16 +227,20 @@ outputdf <- data.frame(
 # what is this file and is it still / will be used?
 # all_files = "/Users/lmcintosh/Downloads/CA004-8.CNV.txt"
 
+parent_dir <- "."
+all_files <- list.files(parent_dir)
+all_files <- all_files[[1]]
 i <- 0
+
 for (file in all_files) {
   try({
-    print(file)
-    i <- i + 1
-    print(i)
-    # UNCOMMENT IN GENERAL
-    # data = try(read.delim(pCaste(parent_dir,file,sep="/")))
-    data <- read.delim(paste(parent_dir, file, sep = "/"))
-    # data = read.delim(file)
+    print(paste("Opening", file))
+
+    # data is a tab-delimited copy number file with the following required
+    # columns: chromosome, start, end, Minor.Copy.Number, Major.Copy.Number
+    data <- read.delim(file.path(parent_dir, file))
+    
+    # skip the file if it is an invalid file
     if (is.null(nrow(data))) {
       print("Could not load this file")
       next
@@ -253,106 +253,80 @@ for (file in all_files) {
     
     data$chromosome <- factor(data$chromosome, levels = chr_levels)
     
-    # now we want to make a simplified version of the data where every chromosomal arm is independent!
-    data$A <- data$Minor.Copy.Number
-    data$B <- data$Major.Copy.Number
-    data$start.pos <- data$start
-    data$end.pos <- data$end
+    # now we want to make a simplified version of the data
+    # where every chromosomal arm is independent!
+    data["A"] <- data["Minor.Copy.Number"]
+    data["B"] <- data["Major.Copy.Number"]
+    data["start.pos"] <- data["start"]
+    data["end.pos"] <- data["end"]
     
-    simple_data <-
-      data[0, c("chromosome", "A", "B", "start.pos", "end.pos")]
+    # create a data frame with the desired columns
+    simple_data <- data[0, c("chromosome", "A", "B", "start.pos", "end.pos")]
     
     for (chr in chr_levels) {
-      chr_data <- data[which(data$chromosome == chr),]
-      chr_data_p <-
-        chr_data[which(chr_data$end.pos < centromeres[which(centromeres$chromosome == chr), "x"]),]
-      chr_data_q <-
-        chr_data[which(chr_data$start.pos > centromeres[which(centromeres$chromosome == chr), "x"]),]
-      chr_data_middle <-
-        chr_data[which(chr_data$start.pos < centromeres[which(centromeres$chromosome == chr), "x"] &
-                         chr_data$end.pos > centromeres[which(centromeres$chromosome == chr), "x"]),]
+      # separate data based on location in a chromosome
+      chr_data <- data[data["chromosome"] == chr, ]
+      chr_data_p <- chr_data[chr_data["end.pos"] <
+                               centromeres[centromeres["chromosome"] == chr, "x"], ]
+      chr_data_q <- chr_data[chr_data["start.pos"] >
+                               centromeres[centromeres["chromosome"] == chr, "x"], ]
+      chr_data_middle <- chr_data[
+        chr_data["start.pos"] < centromeres[centromeres["chromosome"] == chr, "x"] &
+          chr_data["end.pos"] > centromeres[centromeres["chromosome"] == chr, "x"], ]
       
       if (nrow(chr_data_middle) > 0) {
         # that is there is a segment that overlaps the centromere:
         chr_data_middle_p <- chr_data_middle
         chr_data_middle_q <- chr_data_middle
-        chr_data_middle_p$end.pos <-
-          centromeres[which(as.character(centromeres$chromosome) == as.character(chr)), "x"]
-        chr_data_middle_q$start.pos <-
-          centromeres[which(centromeres$chromosome == chr), "x"]
+        chr_data_middle_p["end.pos"] <- centromeres[centromeres["chromosome"] == chr, "x"]
+        chr_data_middle_q["start.pos"] <- centromeres[centromeres["chromosome"] == chr, "x"]
         chr_data_p <- rbind(chr_data_p, chr_data_middle_p)
         chr_data_q <- rbind(chr_data_q, chr_data_middle_q)
       }
       
       # calculate the summary statistics for each arm:
-      meanAp <-
-        sum((chr_data_p$end - chr_data_p$start) * chr_data_p$A) / sum(chr_data_p$end -
-                                                                        chr_data_p$start)
-      meanBp <-
-        sum((chr_data_p$end - chr_data_p$start) * chr_data_p$B) / sum(chr_data_p$end -
-                                                                        chr_data_p$start)
-      startp <- min(chr_data_p$start.pos, na.rm = TRUE)
-      endp <- max(chr_data_p$end.pos, na.rm = TRUE)
+      meanAp <- sum((chr_data_p["end.pos"] - chr_data_p["start.pos"]) * chr_data_p["A"]) /
+        sum(chr_data_p["end.pos"] - chr_data_p["start.pos"])
+      meanBp <- sum((chr_data_p["end.pos"] - chr_data_p["start.pos"]) * chr_data_p["B"]) /
+        sum(chr_data_p["end.pos"] - chr_data_p["start.pos"])
+      startp <- min(chr_data_p["start.pos"], na.rm = TRUE)
+      endp <- max(chr_data_p["end.pos"], na.rm = TRUE)
       
-      meanAq <-
-        sum((chr_data_q$end - chr_data_q$start) * chr_data_q$A) / sum(chr_data_q$end -
-                                                                        chr_data_q$start)
-      meanBq <-
-        sum((chr_data_q$end - chr_data_q$start) * chr_data_q$B) / sum(chr_data_q$end -
-                                                                        chr_data_q$start)
-      startq <- min(chr_data_q$start.pos, na.rm = TRUE)
-      endq <- max(chr_data_q$end.pos, na.rm = TRUE)
+      meanAq <- sum((chr_data_q["end.pos"] - chr_data_q["start.pos"]) * chr_data_q["A"]) /
+        sum(chr_data_q["end.pos"] - chr_data_q["start.pos"])
+      meanBq <- sum((chr_data_q["end.pos"] - chr_data_q["start.pos"]) * chr_data_q["B"]) /
+        sum(chr_data_q["end.pos"] - chr_data_q["start.pos"])
+      startq <- min(chr_data_q["start.pos"], na.rm = TRUE)
+      endq <- max(chr_data_q["end.pos"], na.rm = TRUE)
       
       # put the learnt information in a data frame:
-      simple_data <-
-        rbind(
-          simple_data,
-          data.frame(
-            "chromosome" = chr,
-            "A" = round(meanAp, 0),
-            "B" = round(meanBp, 0),
-            "start.pos" = startp,
-            "end.pos" = endp
-          )
-        )
-      simple_data <-
-        rbind(
-          simple_data,
-          data.frame(
-            "chromosome" = chr,
-            "A" = round(meanAq, 0),
-            "B" = round(meanBq, 0),
-            "start.pos" = startq,
-            "end.pos" = endq
-          )
-        )
+      simple_data <- rbind(simple_data,
+                           data.frame(
+                             "chromosome" = chr,
+                             "A" = round(meanAp),
+                             "B" = round(meanBp),
+                             "start.pos" = startp,
+                             "end.pos" = endp
+                             ))
+      simple_data <- rbind(simple_data,
+                           data.frame(
+                             "chromosome" = chr,
+                             "A" = round(meanAq),
+                             "B" = round(meanBq),
+                             "start.pos" = startq,
+                             "end.pos" = endq
+                             ))
     }
+    
     g1 <- get_CN_track(data, max(data[, c("A", "B")], na.rm = TRUE))
-    simple_data <-
-      simple_data[which(!is.nan(simple_data$A)),] # what is the purpose of this line?
-    g2 <-
-      get_CN_track(simple_data, max(simple_data[, c("A", "B")], na.rm = TRUE))
+    # what is the purpose of this line?
+    simple_data <- simple_data[!is.nan(simple_data["A"]), ]
+    g2 <- get_CN_track(simple_data, max(simple_data[, c("A", "B")], na.rm = TRUE))
+    
     print(grid.arrange(g1, g2))
     
-    # simple_data$zero = apply(simple_data[,c("A","B")],1,min)
-    #
-    # zero_data = simple_data[which(simple_data$zero == 0),]
-    # non_zero_data = simple_data[which(simple_data$zero > 0),]
-    #
-    # X = c(non_zero_data[,"A"],non_zero_data[,"B"],apply(zero_data[,c("A","B")],1,min))
-    #
-    # sizeX = length(X)
-    # X <- table(X) #[which(X>0)])
-    # X <- X/sum(X)
-    
-    X <- table(c(simple_data[, c("A", "B")])) #[which(X>0)])
+    X <- table(c(simple_data[, c("A", "B")]))
     X <- X / sum(X)
-    X#
-    # # Xzero are the ones that are in their final state protected from zero.
-    # Xzero = apply(zero_data[,c("A","B")],1,max)
-    # sizeXzero = length(Xzero)
-    # Xzero <- table(X) #[which(X>0)])
-    # Xzero <- X/sum(Xzero)
     
     alphamax_nogd <- 0
     lmax_nogd <- -Inf
@@ -364,109 +338,90 @@ for (file in all_files) {
     lmax_half <- -Inf
     
     for (alpha in 1:10 / 50) {
-      # likes = likelihoodGD(alpha,X,sizeX,Xzero,sizeXzero)
-      # likes_half = likelihoodhalfGD(alpha,simple_data)
-      
-      likes <- likelihoodGDany(alpha, simple_data, half = F, FA = F)
-      likes_half <-
-        likelihoodGDany(alpha, simple_data, half = T, FA = F)
+
+      likes <- likelihoodGDany(alpha, simple_data, half = FALSE, FA = FALSE)
+      likes_half <- likelihoodGDany(alpha, simple_data, half = TRUE, FA = FALSE)
       
       l <- max(likes[, 2:ncol(likes)], na.rm = TRUE)
       l_nogd <- max(likes[, 1], na.rm = TRUE)
-      
       l_half <- max(likes_half, na.rm = TRUE)
+      
       if (l > lmax) {
         lmax <- l
         alphamax <- alpha
-        indexmax <-
-          which(likes == max(likes[, 2:ncol(likes)], na.rm = TRUE), arr.ind = TRUE)
+        indexmax <- which(likes == max(likes[, 2:ncol(likes)], na.rm = TRUE), arr.ind = TRUE)
         # the first row is for no GD....
       }
+      
       if (l_half > lmax_half) {
         lmax_half <- l_half
         alphamax_half <- alpha
-        indexmax_half <-
-          which(likes_half == max(likes_half, na.rm = TRUE), arr.ind = TRUE)
+        indexmax_half <- which(likes_half == max(likes_half, na.rm = TRUE), arr.ind = TRUE)
       }
+      
       if (l_nogd > lmax_nogd) {
         lmax_nogd <- l_nogd
         alphamax_nogd <- alpha
-        indexmax_nogd <-
-          which(likes == max(likes[, 1], na.rm = TRUE), arr.ind = TRUE)
+        indexmax_nogd <- which(likes == max(likes[, 1], na.rm = TRUE), arr.ind = TRUE)
       }
+      
       print(c(lmax, alphamax, indexmax[1] - 1, indexmax[2] - 2))
-      print(c(
-        lmax_half,
-        alphamax_half,
-        indexmax_half[1] - 1,
-        indexmax_half[2] - 2
-      ))
+      print(c(lmax_half, alphamax_half, indexmax_half[1] - 1, indexmax_half[2] - 2))
       print(c(lmax_nogd, alphamax_nogd, indexmax_nogd[1] - 1))
-      # print(likes)
-      # print(likes_half)
       print(alpha)
       print(X)
     }
     
     print(c(lmax, alphamax, indexmax[1] - 1, indexmax[2] - 1))
-    print(c(
-      lmax_half,
-      alphamax_half,
-      indexmax_half[1] - 1,
-      indexmax_half[2] - 2
-    ))
+    print(c(lmax_half, alphamax_half, indexmax_half[1] - 1, indexmax_half[2] - 2))
     print(c(lmax_nogd, alphamax_nogd, indexmax_nogd[1] - 1))
     print(X)
     
-    outputdf <- rbind(
-      outputdf,
-      data.frame(
-        "name" = file,
-        "lmax" = lmax,
-        "alphamax" = alphamax,
-        "indexmaxN" = indexmax[1] - 1,
-        "indexmaxM" = indexmax[2] - 2,
-        "lmax_half" = lmax_half,
-        "alphamax_half" = alphamax_half,
-        "indexmaxN_half" = indexmax_half[1] -
-          1,
-        "indexmaxM_half" = indexmax_half[2] - 2,
-        "lmax_nogd" = lmax_nogd,
-        "alphamax_nogd" = alphamax_nogd,
-        "indexmax_nogd" = indexmax_nogd[1] - 1
-      )
-    )
+    outputdf <- rbind(outputdf,
+                      data.frame(
+                        "name" = file,
+                        "lmax" = lmax,
+                        "alphamax" = alphamax,
+                        "indexmaxN" = indexmax[1] - 1,
+                        "indexmaxM" = indexmax[2] - 2,
+                        "lmax_half" = lmax_half,
+                        "alphamax_half" = alphamax_half,
+                        "indexmaxN_half" = indexmax_half[1] - 1,
+                        "indexmaxM_half" = indexmax_half[2] - 2,
+                        "lmax_nogd" = lmax_nogd,
+                        "alphamax_nogd" = alphamax_nogd,
+                        "indexmax_nogd" = indexmax_nogd[1] - 1
+                        ))
     
     if (lmax > lmax_nogd & lmax > lmax_half) {
       print("the GD")
     } else if (lmax_half > lmax_nogd & lmax_half > lmax) {
       print("half the GD")
-    } else{
+    } else {
       print("no GD")
     }
   })
 }
+
 outputdf <- outputdf[, 1:12]
 colnames(outputdf)
-outputdf$GD <-
-  outputdf$lmax > outputdf$lmax_nogd# & outputdf$lmax > outputdf$lmax_half
-# outputdf$GD_half <- outputdf$lmax_half > outputdf$lmax_nogd & outputdf$lmax_half > outputdf$lmax
-outputdf$no_GD <-
-  outputdf$lmax_nogd >= outputdf$lmax #& outputdf$lmax_nogd > outputdf$lmax_half
+outputdf["GD"] <- outputdf["lmax"] > outputdf["lmax_nogd"]
+outputdf["no_GD"] <- outputdf["lmax_nogd"] >= outputdf["lmax"]
 
-c(sum(outputdf$GD), sum(outputdf$GD_half), sum(outputdf$no_GD))
-
+# this isn't assigned to anything, safe to delete?
+c(sum(outputdf["GD"]), sum(outputdf["GD_half"]), sum(outputdf["no_GD"]))
+# this isn't assigned to anything, safe to delete?
 outputdf[, c("lmax", "lmax_nogd", "GD", "no_GD")]
 
+# what is k?
+# n is not used, safe to delete?
 k <- 3
 n <- 92
-outputdf$AIC_full <- 2 * k - 2 * outputdf$lmax
-# outputdf$AIC_half <- 2*(k+23)-2*outputdf$lmax_half
-outputdf$AIC_null <- 2 * (k - 1) - 2 * outputdf$lmax_nogd
+outputdf["AIC_full"] <- 2 * k - 2 * outputdf["lmax"]
+outputdf["AIC_null"] <- 2 * (k - 1) - 2 * outputdf["lmax_nogd"]
 
-outputdf$GD_AIC <- outputdf$AIC_full <  outputdf$AIC_null
-# outputdf$GD_half_AIC <- outputdf$AIC_half < outputdf$AIC_full & outputdf$AIC_half < outputdf$AIC_null
-outputdf$no_GD_AIC <-  outputdf$AIC_null <= outputdf$AIC_full
+outputdf["GD_AIC"] <- outputdf["AIC_full"] < outputdf["AIC_null"]
+outputdf["no_GD_AIC"] <- outputdf["AIC_null"] <= outputdf["AIC_full"]
 
 # AIC is founded on information theory:
 # it offers a relative estimate of the information lost
@@ -476,11 +431,10 @@ outputdf$no_GD_AIC <-  outputdf$AIC_null <= outputdf$AIC_full
 # the model. We choose the candidate model that minimized
 # the information loss.
 
-outputdf$bestAIC <-
-  apply(outputdf[, c("AIC_full", "AIC_null")], 1, min)
-outputdf$rl_GD <- exp((outputdf$bestAIC - outputdf$AIC_full) / 2)
-outputdf$rl_noGD <- exp((outputdf$bestAIC - outputdf$AIC_null) / 2)
+outputdf["bestAIC"] <- apply(outputdf[, c("AIC_full", "AIC_null")], 1, min)
+outputdf["rl_GD"] <- exp((outputdf["bestAIC"] - outputdf["AIC_full"]) / 2)
+outputdf["rl_noGD"] <- exp((outputdf["bestAIC"] - outputdf["AIC_null"]) / 2)
 
-write.table(outputdf, file = paste0("/Users/lmcintosh", "/outputdf.txt"))
+write.table(outputdf, file = "outputdf.txt")
 
-all_files[which(!(all_files %in% outputdf$name))]
+all_files[!(all_files %in% outputdf["name"])]
